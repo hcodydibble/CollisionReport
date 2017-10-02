a3b275ff593190e0c3f8114521bc0e8e05552fa9
Shannon Dillon <sedillon93@gmail.com>
2017-10-02 11:52:08 -0700

    add second circle for other person's car; both are draggable

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,30 +4,39 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 20,
+    zoom: 21,
   });
   //only need the infoWindow if we want to display something over the map, like a message (see code above map.setCenter(pos))
   // infoWindow = new google.maps.InfoWindow;
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      //navigator is an object created by Google
-      //navigator.geolocation returns an object called Geolocation which has three functions (one of which is getCurrentPosition)
-      //position is an object; coordinates is a key whose value is an array of lat, long, altitude, speed, accuracy, and heading
+
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
-        var circle = new google.maps.Circle({
-          strokeColor: '#FF0000',
+        var myCar = new google.maps.Circle({
+          strokeColor: 'blue',
           strokeOpacity: 0.8,
           strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35,
+          fillColor: 'blue',
+          fillOpacity: 0.5,
           map: map,
           center: {lat: position.coords.latitude, lng: position.coords.longitude},
-          radius: 2,
+          radius: 1.5,
+          draggable: true
+        });
+        var otherCar = new google.maps.Circle({
+          strokeColor: 'red',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: 'red',
+          fillOpacity: 0.5,
+          map: map,
+          center: {lat: position.coords.latitude, lng: position.coords.longitude},
+          radius: 1.5,
           draggable: true
         });
 
