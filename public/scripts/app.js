4a5f251b64e48ce60f1e29d6f3e1131cb31a21b4
Shannon Dillon <sedillon93@gmail.com>
2017-10-02 16:24:10 -0700

    harvesting the latitude from markers placed by the user!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,34 +9,34 @@ function initMap() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
-        var myCar = new google.maps.Circle({
-          strokeColor: 'blue',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: 'blue',
-          fillOpacity: 0.5,
-          map: map,
-          center: {lat: position.coords.latitude, lng: position.coords.longitude},
-          radius: 1,
-          draggable: true
-        });
-        var otherCar = new google.maps.Circle({
-          strokeColor: 'red',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: 'red',
-          fillOpacity: 0.5,
-          map: map,
-          center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
-          radius: 1,
-          draggable: true
-        });
+
+        // var myCar = new google.maps.Circle({
+        //   strokeColor: 'blue',
+        //   strokeOpacity: 0.8,
+        //   strokeWeight: 2,
+        //   fillColor: 'blue',
+        //   fillOpacity: 0.5,
+        //   map: map,
+        //   center: {lat: position.coords.latitude, lng: position.coords.longitude},
+        //   radius: 1,
+        //   draggable: true
+        // });
+        // var otherCar = new google.maps.Circle({
+        //   strokeColor: 'red',
+        //   strokeOpacity: 0.8,
+        //   strokeWeight: 2,
+        //   fillColor: 'red',
+        //   fillOpacity: 0.5,
+        //   map: map,
+        //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
+        //   radius: 1,
+        //   draggable: true
+        // });
         // var arrow = new google.maps.Marker({
         //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
         //   map: map,
@@ -50,5 +50,19 @@ function initMap() {
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  map.addListener('click', function(e) {
+    placeMarkerAndPanTo(e.latLng, map);
+  });
+
+  function placeMarkerAndPanTo(latLng, map) {
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+    //this code (latLng.lat().toFixed(3) gets the latitude for wherever the user placed the marker)
+    console.log(latLng.lat().toFixed(3));
+    map.panTo(latLng);
   }
 }
