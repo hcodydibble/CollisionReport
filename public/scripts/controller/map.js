9056a218c2d97081cf9aa52f1488b2433d3be45d
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-02 17:11:45 -0700

    deleted map.js, moved script tags to bottom. deleted screenshot stuff in server.js

--- public/scripts/controller/map.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function(){
-  let addForwardArrow = function(){
-    $('#forward_arrow').on('click', function(){
-      $('#map').append(google.maps.SymbolPath.FORWARD_CLOSED_ARROW);
-    }
-  })
-})()
--- server.js
+++ server.js
@@ -1,27 +1,12 @@
 'use strict';
 
 const EXPRESS = require('express');
-const PARSER = require('body-parser');
 const FS = require('fs');
 const REQUEST = require('request');
 const APP = EXPRESS();
 const PORT = process.env.PORT || 3000;
 
 APP.use(EXPRESS.static('public'));
-
-APP.post('/screenshot', function(req,res){
-  REQUEST({
-    url: 'https://apileap.com/api/screenshot/v1/urltoimage',
-    encoding: 'binary',
-    qs: {
-      url: 'http://localhost:3000/',
-      access_key: '3165e1148d1e4e9fbe59b8ce6f0cd350'
-    }
-  }, function(error,response,body){
-    console.log(body)
-    FS.writeFile('public/img/test.jpeg', body, 'binary', function(err){console.log(err)});
-  });
-})
 
 APP.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
 
