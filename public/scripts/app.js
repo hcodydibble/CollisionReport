f7672baba1e73462af60b1e21774650dd67420da
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-03 08:57:39 -0700

    TESTS! jk just added lat and long variables to change the center of our map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,31 @@
 'use strict';
 
+//this takes the users input of address and turns it into lat,long
+var lati = 47.5739918;
+var long = -122.41761162;
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: lati, lng: long},
     zoom: 21,
   });
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-        // var myCar = new google.maps.Circle({
-        //   strokeColor: 'blue',
-        //   strokeOpacity: 0.8,
-        //   strokeWeight: 2,
-        //   fillColor: 'blue',
-        //   fillOpacity: 0.5,
-        //   map: map,
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude},
-        //   radius: 1,
-        //   draggable: true
-        // });
-        // var otherCar = new google.maps.Circle({
-        //   strokeColor: 'red',
-        //   strokeOpacity: 0.8,
-        //   strokeWeight: 2,
-        //   fillColor: 'red',
-        //   fillOpacity: 0.5,
-        //   map: map,
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
-        //   radius: 1,
-        //   draggable: true
-        // });
-        // var arrow = new google.maps.Marker({
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
-        //   map: map,
-        //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
-        // })
-
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, map.getCenter());
-  }
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     var pos = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude
+  //     };
+  //
+  //     map.setCenter(pos);
+  //   }, function() {
+  //     handleLocationError(true, map.getCenter());
+  //   });
+  // } else {
+  //   // Browser doesn't support Geolocation
+  //   handleLocationError(false, map.getCenter());
+  // }
 
   map.addListener('click', function(e) {
     placeMarker(e.latLng, map);
