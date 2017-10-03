f21a201df355c8fad7fca4d051ffcd8193fe1b6c
Shannon Dillon <sedillon93@gmail.com>
2017-10-03 07:31:03 -0700

    close a tags so paragraph isn't a link anymore

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var markers = [];
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -13,34 +14,6 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
-        // var myCar = new google.maps.Circle({
-        //   strokeColor: 'blue',
-        //   strokeOpacity: 0.8,
-        //   strokeWeight: 2,
-        //   fillColor: 'blue',
-        //   fillOpacity: 0.5,
-        //   map: map,
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude},
-        //   radius: 1,
-        //   draggable: true
-        // });
-        // var otherCar = new google.maps.Circle({
-        //   strokeColor: 'red',
-        //   strokeOpacity: 0.8,
-        //   strokeWeight: 2,
-        //   fillColor: 'red',
-        //   fillOpacity: 0.5,
-        //   map: map,
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
-        //   radius: 1,
-        //   draggable: true
-        // });
-        // var arrow = new google.maps.Marker({
-        //   center: {lat: position.coords.latitude, lng: position.coords.longitude + 0.00003},
-        //   map: map,
-        //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
-        // })
 
       map.setCenter(pos);
     }, function() {
@@ -60,7 +33,10 @@ function initMap() {
       position: latLng,
       map: map
     });
-    //this code (latLng.lat().toFixed(3) gets the latitude for wherever the user placed the marker)
+    markers.push(marker);
+    //to access a specific marker's lat/long: markers[0].getPosition().lat() OR markers[0].getPosition().lng()
+    //this code (latLng.lat().toFixed(6) gets the latitude for wherever the user placed the marker to the six decimal place)
+    //remove marker at specific position with markers[0].setMap(null); will need to adjust array length b/c will remove marker from map NOT from marker array (use above code in conjunction w/ arr.pop to remove from map)
     console.log(latLng.lat().toFixed(6));
     console.log(latLng.lng().toFixed(6));
   }
