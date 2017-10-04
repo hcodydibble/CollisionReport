25b3c3827f79eabb0e0a70baaf8f752067fe5e33
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-03 17:34:28 -0700

    heroku test

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 'use strict';
 
 const EXPRESS = require('express');
-const FS = require('fs');
-const REQUEST = require('request');
+const MAILER = require('nodemailer');
 const APP = EXPRESS();
 const PORT = process.env.PORT || 3000;
+const TRANSPORTER = MAILER.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hcodydibble@gmail.com',
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
 
 APP.use(EXPRESS.static('public'));
+
+APP.post('/mail',function(req,res){
+  TRANSPORTER.sendMail(req,function(error,info){
+    if(error){
+      console.log(error)
+    }else{
+      console.log('Email sent: ' + info.response)
+    }
+  })
+})
 
 APP.get('*', (request, response) => response.sendFile('index.html', {root: './public'}));
 
