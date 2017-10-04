e53854faf18b47a0703a3594e15debe15a9dce71
Shannon Dillon <sedillon93@gmail.com>
2017-10-04 11:26:22 -0700

    fix car drop down menu ONE MORE TIME

--- public/scripts/view/mapView.js
+++ public/scripts/view/mapView.js
@@ -3,7 +3,7 @@
 function mapView() {
   if ($('#map').is(':empty')) {initMap()}
   $('.mapPage').show().siblings().hide();
-  $('#carSelect').on('click', () => {
-    $('#carIcons').toggle(500);
-  })
 }
+$('#carSelect').on('click', () => {
+  $('#carIcons').toggle(500);
+})
