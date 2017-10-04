778f1b8af8edf5e81e55dfe9fb15609c174353d5
Shannon Dillon <sedillon93@gmail.com>
2017-10-04 15:51:06 -0700

    revise map image sizing

--- public/scripts/controller/mapController.js
+++ public/scripts/controller/mapController.js
@@ -67,7 +67,7 @@ $('#saveMap').on('click', () => {
     markerUrls += '&markers=icon:' + marker.path + '|' + marker.lat + ',' + marker.lng;
     return markerUrls;
   })
-  $('#testImage').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${mapCenter.lat},${mapCenter.lng}&zoom=20&size=700x300${markerUrls}&key=AIzaSyD-PrvzwpOWXJ7A2TRqspmdyHQlA7F1_5k`)
+  $('#testImage').attr('src', `https://maps.googleapis.com/maps/api/staticmap?center=${mapCenter.lat},${mapCenter.lng}&zoom=${map.getZoom()}&size=700x600${markerUrls}&key=AIzaSyD-PrvzwpOWXJ7A2TRqspmdyHQlA7F1_5k`)
   notesView();
 })
 
@@ -76,7 +76,7 @@ $('#emailButton').on('click', () => {
     from: `"${$('#userName').val()}"<reportmywreck@gmail.com>`,
     to: `${$('#recipient').val()}`,
     subject: `${$('#subject').val()}`,
-    html: `<p>${$('#emailBody').val()}</p><img src='https://maps.googleapis.com/maps/api/staticmap?center=${mapCenter.lat},${mapCenter.lng}&zoom=20&size=600x600${markerUrls}&key=AIzaSyD-PrvzwpOWXJ7A2TRqspmdyHQlA7F1_5k'>`
+    html: `<p>${$('#emailBody').val()}</p><img src='https://maps.googleapis.com/maps/api/staticmap?center=${mapCenter.lat},${mapCenter.lng}&zoom=${map.getZoom()}&size=700x600${markerUrls}&key=AIzaSyD-PrvzwpOWXJ7A2TRqspmdyHQlA7F1_5k'>`
   }
   if($('#recipient').val() !== '' && $('#recipient').val().includes('@' && '.')){
     $.post('/mail', mailOptions)
